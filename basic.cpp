580d38a9caafd90bf6100b904972db58dc97daf0
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-11-30 21:29:56 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,25 @@
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+  //  char name[]="NIMISH";
+    cout<<"Enter Age "<<x<<endl;
+    cin>>x;
+    //cout<<name<<" you are "<<x<<" years old"<<endl;
+    cout<<"you are "<<x<<"years old."<<endl;
+    return 0;
+}*/
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main( ) {
+   char sample[] = "GeeksforGeeks";
+ 
+   cout << sample << " - A computer science portal for geeks";
+   
+   return 0;
+}
