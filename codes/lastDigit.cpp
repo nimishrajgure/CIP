bf46398936218c551b36af4ccde0b89a8646579e
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-12-02 23:26:32 +0530

    Create lastDigit.cpp

---
+++ codes/lastDigit.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	int x = 235;
+	
+	int y = -256;
+	
+	int ans = x % 10;
+	
+	int ans1 = y % 10;
+	
+	int ans2 = abs(y) % 10;
+	
+	cout << ans << endl;
+	
+	cout << ans1 << endl;
+	
+	cout << ans2;
+	
+	return 0;
+}
