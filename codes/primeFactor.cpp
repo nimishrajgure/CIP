d83e0b200cb534846ae5de83f323e0af1fa084e0
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-12-05 23:20:17 +0530

    Create primeFactor.cpp

---
+++ codes/primeFactor.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    for(int i = 2 ; i < n ; i ++) 
+    {
+        if(n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int primeFactors(int n)
+{
+    for(int i = 2; i <= n ; i ++)
+    {
+        if(isPrime(i))
+        {
+            int x = i ; 
+            while(n%x==0)
+            {
+                cout << i << " " ;
+                x = x*i ; 
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    primeFactors(n);
+    return 0;
+}
