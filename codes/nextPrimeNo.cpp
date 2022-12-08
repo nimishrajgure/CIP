620afa66dec799b84cf0ed5afa440862eced9e3a
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-12-08 13:32:23 +0530

    Update nextPrimeNo.cpp

--- codes/nextPrimeNo.cpp
+++ codes/nextPrimeNo.cpp
@@ -1,65 +1,26 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-// } Driver Code Ends
-//User function Template for C++
-
-bool isPrime(int n)
-{
-    // Corner cases
-    if (n <= 1)  return false;
-    if (n <= 3)  return true;
-   
-    // This is checked so that we can skip 
-    // middle five numbers in below loop
-    if (n%2 == 0 || n%3 == 0) return false;
-   
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return false;
-   
-    return true;
-}
- 
-// Function to return the smallest
-// prime number greater than N
-int nextPrime(int N)
-{
- 
-    // Base case
-    if (N <= 1)
-        return 2;
- 
-    int prime = N;
-    bool found = false;
- 
-    // Loop continuously until isPrime returns
-    // true for a number greater than n
-    while (!found) {
-        prime++;
- 
-        if (isPrime(prime))
-            found = true;
+int nextPrime(int n){
+    n++;
+    while(true){
+        int i;
+        for(i = 2; i < n; i++){
+            if(n % i == 0)
+                break;
+        }
+        if(i == n)
+            return n;
+        n++;
     }
- 
-    return prime;
 }
- 
 
-//{ Driver Code Starts.
+int main()
+{
+    int n;
+    cin >> n;
     
-int main() {
-	
-	int t; cin>>t;
-	
-	while(t--){
-	    int n; cin>>n;
-        int ans = nextPrime(n);
-        cout<<ans<<endl;
-	}
+    cout << nextPrime(n);
+    
+    return 0;
 }
-// } Driver Code Ends
