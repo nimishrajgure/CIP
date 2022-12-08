fe834c8eaedf1658817a7b6324a07394fe40a47c
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-12-08 13:29:35 +0530

    Create decimalToBInary.cpp

---
+++ codes/decimalToBInary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<bool>v;
+    while(n > 0)
+    {
+        int t = n % 2;
+        v.push_back(t);
+        n /= 2;
+    }
+    for(int i = v.size() - 1; i >= 0; i--)
+        cout << v[i];
+    return 0;
+}
