8361112d40101a3b88300fbecca1c96e6665d8cf
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2022-12-30 18:01:47 +0530

    Create reverseStr.cpp

---
+++ codes/reverseStr.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string reverseWord(string str);
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;
+	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+string reverseWord(string str){
+    
+  string B;
+  int i;
+  for(i=str.length()-1;i>=0;i--)
+  {
+      B+=str.at(i);
+  }
+  return B;
+}
