3b5418b16231f36e8967e0f3dfd88838fc03af37
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2023-01-02 23:56:00 +0530

    Create unionStruct.cpp

---
+++ codes/unionStruct.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+/*create binary tree struct node*/
+
+struct NODE 
+
+{
+
+    double data;
+
+    union{
+
+            NODE *left;
+
+            NODE *right;
+
+        
+
+    };
+
+};// your code here
+
+//{ Driver Code Starts.
+#include <bits/stdc++.h>
+int main() {
+	//code
+	
+	struct NODE ll;
+	printf("%d", 1);
+	return 0;
+}
+// } Driver Code Ends
