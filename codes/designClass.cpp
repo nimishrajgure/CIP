580307b4c1ab5442ee0a7bd3bc52afe535c95493
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2023-02-01 23:53:19 +0530

    Create designClass.cpp

---
+++ codes/designClass.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// Design a class as described in the question
+// Code here
+class MyClass
+{
+    public:
+        
+        
+    void display(){
+        cout<<"Hello World"<<endl;
+    }    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        MyClass obj;
+        obj.display();
+    }
+    return 0;
+}
+
+// } Driver Code Ends
