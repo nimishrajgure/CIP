62f02c35ca10c1e0724cf6c26ed8f61f210e0b90
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2023-02-01 23:56:15 +0530

    Create constructor.cpp

---
+++ codes/constructor.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// design a class User 
+// instance variable: name(String)
+// Constructor: no parameter, initialize name to "Default"
+
+
+// code here
+class User
+{
+    public:
+    
+    string name;
+    
+    User()
+    {
+        name = "Default";
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        User user;
+        cout<<user.name<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
