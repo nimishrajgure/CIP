f45eddf2f582869919160100b8ae445ce5dcdfbf
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2023-02-05 23:55:49 +0530

    Create inheritenceIntro.cpp

---
+++ codes/inheritenceIntro.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Cuboid 
+{
+   //Add your code here.
+   public:
+   int l,b,h;
+   void display()
+   {
+       cout<<l<<" "<<b<<" "<<h<<endl;
+   }
+};
+
+class CuboidVol: public Cuboid
+{
+    //Add your code here
+    public:
+     void read_input(int l, int b, int h)
+
+   {
+
+       this->l = l;
+
+       this->b=b;
+
+       this->h=h;
+
+   }
+
+   void display()
+
+   {
+
+       cout<<l*b*h<<endl;
+
+   }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int l, w, h;
+        cin>>l>>w>>h;
+    
+    // Declare a CuboidVol object
+    
+    CuboidVol c_vol;
+    
+    
+    // Read length width and height
+     
+    c_vol.read_input(l,w,h);
+    
+    
+    //  Print length, width and height
+    
+    c_vol.Cuboid::display();
+    
+    
+    // Print the Volume
+     
+    c_vol.display();
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
