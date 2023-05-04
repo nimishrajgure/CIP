d91e6780112bba56c9c29e345b45fcec5133e3c1
nimishrajgure <99610471+nimishrajgure@users.noreply.github.com>
2023-05-04 19:03:27 +0530

    Create Print Array Elements Using Recursion.cpp

---
+++ DSA/recursion/Print Array Elements Using Recursion.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        void printArrayRecursively(int arr[],int n)
+    {
+       //Your code here
+       //Use recursion to print array elements from start to end
+       if(n==0) return;
+       if(n==1){
+           cout<<arr[0]<<" ";
+           return;}
+       printArrayRecursively(arr,n-1);
+       cout<<arr[n-1]<<" ";
+                    
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--) 
+	{
+	    int N;
+	    cin>>N;
+	    int arr[N];
+	    for(int i=0;i<N;i++)
+	    cin>>arr[i];
+	    Solution ob;
+	    ob.printArrayRecursively(arr,N);
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
